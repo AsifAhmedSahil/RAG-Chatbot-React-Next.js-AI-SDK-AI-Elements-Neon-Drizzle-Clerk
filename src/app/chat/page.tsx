02831fcaf76b909b41007a5ae13154de0195086b
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-12-11 09:57:36 +0600

    update

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -20,32 +20,58 @@ import { Response } from "@/components/ai-elements/response";
 import { Loader } from "@/components/ai-elements/loader";
 
 export default function RAGChatBot() {
-    const [input,setInput] = useState("")
-    const {messages,sendMessage,status} = useChat()
+  const [input, setInput] = useState("");
+  const { messages, sendMessage, status } = useChat();
+
+  const handleSubmit = (message: PromptInputMessage) => {
+    if (!message.text) return;
+    sendMessage({ text: message.text });
+    setInput("");
+  };
   return (
     <div className="max-w-4xl mx-auto p-6 relative size-full h-[calc(100vh)]">
       <div className="flex flex-col h-full">
         <Conversation className="h-full">
           <ConversationContent>
-            {
-                messages.map()
-            }
+            {messages.map((message) => (
+              <div key={message.id}>
+                {message.parts.map((part, i) => {
+                  switch (part.type) {
+                    case "text":
+                      return (
+                        <Fragment key={`${message.id}-${i}`}>
+                          <Message from={message.role}>
+                            <MessageContent>
+                              <Response>{part.text}</Response>
+                            </MessageContent>
+                          </Message>
+                        </Fragment>
+                      );
+                    default:
+                      return null;
+                  }
+                })}
+              </div>
+            ))}
+            {(status === "submitted" || status === "streaming") && <Loader />}
           </ConversationContent>
           <ConversationScrollButton />
         </Conversation>
 
-        <PromptInput className="mt-4">
-            <PromptInputBody>
-                <PromptInputTextarea value={input} onChange={(e)=> setInput(e.target.value)}/>
-            </PromptInputBody>
+        <PromptInput onSubmit={handleSubmit} className="mt-4">
+          <PromptInputBody>
+            <PromptInputTextarea
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </PromptInputBody>
 
-            <PromptInputToolbar>
-                <PromptInputTools>
-                    {/* model selector web search etc */}
-                </PromptInputTools>
-                <PromptInputSubmit/>
-            </PromptInputToolbar>
-
+          <PromptInputToolbar>
+            <PromptInputTools>
+              {/* model selector web search etc */}
+            </PromptInputTools>
+            <PromptInputSubmit />
+          </PromptInputToolbar>
         </PromptInput>
       </div>
     </div>
