de3905a61bbde3b4f522676fc0dd024b46a6f734
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-12-12 17:19:40 +0600

    single embedding and multiple embedding function geneted

---
+++ src/lib/embeddings.ts
@@ -0,0 +1,26 @@
+import { openai } from "@ai-sdk/openai";
+import { embed, embedMany } from "ai";
+
+export async function generateEmbedding(text: string) {
+  const input = text.replace("\n", " ");
+
+  const { embedding } = await embed({
+    model: openai.textEmbeddingModel("text-embedding-3-small"),
+    value: input,
+  });
+
+  return embedding;
+}
+
+// for processing pdf - embedded many
+
+export async function generateEmbeddings(texts: string[]) {
+  const inputs = texts.map((text)=> text.replace("\n", " "));
+
+  const { embeddings } = await embedMany({
+    model: openai.textEmbeddingModel("text-embedding-3-small"),
+    values: inputs,
+  });
+
+  return embeddings;
+}
